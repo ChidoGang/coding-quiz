2e10d066a6327cc276f92eaab66f8ed1961c82ac
ChidoGang <edgar_quintanilla@mymail.rcbc.edu>
2021-09-20 01:42:22 -0400

    Edited the file to meet the exact critera
    Edited the javascript code to have the button properly functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,142 +1,193 @@
-var main = document.querySelector("#main");
-var homepage = document.querySelector("#landing-page");
-var start = document.querySelector("#start");
-var spanTime = document.querySelector("#time");
-var exitPage = document.querySelector(".exit-page");
-var spanFinalScore = document.querySelector("#final-score");
-var submitName = document.querySelector("#submit-name");
-var initials = document.querySelector("#initials");
-var highscore = document.querySelector("#highscore");
+/* Functions for the Startbtn, Load a Question and hide the start button */
 
+const startBtn = document.getElementById("startBtn");
+const highscoreBtn = document.getElementById("highscoreBtn");
+const submitBtn = document.getElementById("submitBtn");
+const homeBtn = document.getElementById("homeBtn");
+const clearBtn = document.getElementById("clearBtn");
 
-var quesArr=[
+/* Screen display for each page */
+const introScreen = document.getElementById("introScreen");
+const quizScreen = document.getElementById("quizScreen");
+const submitScreen = document.getElementById("submitScreen");
+const highscoreScreen = document.getElementById("highscoreScreen");
+
+/* Text display inputs the text into Html */
+const scoreEl = document.getElementById("scoreEl");
+const timerEl = document.getElementById("timerEl");
+const question = document.getElementById("question");
+const answer1 = document.getElementById("answer1");
+const answer2 = document.getElementById("answer2");
+const answer3 = document.getElementById("answer3");
+const answer4 = document.getElementById("answer4");
+const initialEl = document.getElementById("initialEl");
+const finalscoreEl = document.getElementById("finalscoreEl");
+const highscoreList = document.getElementById("highscoreList");
+
+var currentQuestion = 0;
+var questionCounter = 1;
+var score = 0;
+
+/* Questions Which include an array of questions */
+let questions = [
      {
-          ques: "String values must be enclosed within ___ when being assigned to variables?",
-          ans: ["commas", "curly brackets", "quotes", "paraenthesis"],
-          correctAns: "3",
+          question: "String values must be enclosed within ___ when being assigned to variables?",
+          answer1: "commas", 
+          answer2: "curly brackets", 
+          answer3: "quotes", 
+          answer4: "paraenthesis",
+          correct: "3"
      },
-
      {
-          ques: "Arrays in JavaScript can be used to store ?",
-          ans: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-          correctAns: "4",
+          question: "Arrays in JavaScript can be used to store ?",
+          answer1: "numbers and strings", 
+          answer2: "other arrays", 
+          answer3: "booleans", 
+          answer4: "all of the above",
+          correct: "4"
      },
-
      {
-          ques: "Which of these is NOT a programming language ?",
-          ans: ["ISO", "pYTHON", "Java", "HTML"],
-          correctAns: "1",
+          question: "Which of these is NOT a programming language ?",
+          answer1: "ISO", 
+          answer2: "PYTHON", 
+          answer3: "Java", 
+          answer4: "HTML",
+          correct: "1",
      },
-
      {
-          ques: "What are people who write computer code called?",
-          ans: ["programmers","Cryptographers","Professors","Technicians"],
-          correctAns: "1",
+          question: "What are people who write computer code called?",
+          answer1: "programmers",
+          answer2: "Cryptographers",
+          answer3: "Professors",
+          answer4: "Technicians",
+          correct: "1",
      },
-
      {
-          ques: "What is computer coding ?",
-          ans: ["A TV show", "A radio wave ", "telling a computer what to do", "A list of functions"],
-          correctAns: "3",
+          question: "What is computer coding ?",
+          answer1: "A TV show", 
+          answer2: "A radio wave ", 
+          answer3: "telling a computer what to do",
+          answer4: "A list of functions",
+          correct: "3",
      }
 ];
-var index = 0;
-var timer = parseInt(quesArr.length*10);
-var timeInterval;
+/*End of multiple choice questions */
 
-spanTime.textContent = timer;
-
-var score = [];
-
-
-submitName.addEventListener("submit", function(event){
-
-     event.preventDefault();
-     console.log("sumit score");
-     console.log(initials.value);
-
-     score.push({"name": initials.value, "score": spanFinalScore.textContent});
-     localStorage.setItem("score", JSON.stringify(score));
-
-     exitPage.classList.add("hide");
-     homepage.classList.remove("hide");
-
+/*Start of function*/ 
+function startQuiz() {
+     scoreEl.innerHTML = "Score: " + 0;
+     timerEl.innerHTML = "Time: " + 75;
+     score = 0;
      index = 0;
-     timer = parseInt(quesArr.length*15);
-     spanTime.textContent = timer;
-});
-
-
-function startTime(){
-     timeInterval = setInterval(function(){
-
-          timer--;
-          spanTime.textContent = timer;
-
-          if(index === quesArr.length || timer < 0){
-               clearInterval(timeInterval);
-               spanFinalScore.textContent = timer;
-               exitPage.classList.remove("hide");
-          }
-     }, 1000);
+     questionCounter = 1;
+     introScreen.style.display = "none";
+     questionPage();
+     timer();
+     quizScreen.style.display = "flex";
+     scoreEl.style.display = "block";
+     timerEl.style.display = "block";
 }
 
-function createAndLoadQues(){
-     var div = document.createElement("div");
-     main.appendChild(div);
-     div.setAttribute("class", "ques");
-
-     var h1 = document.createElement("h1");
-     div.appendChild(h1);
-     h1.textContent = quesArr[index].ques;
-
-     var btnsDiv = document.createElement("div");
-     div.appendChild(btnsDiv);
-     btnsDiv.setAttribute("class", "choices")
-
-     var answers = quesArr[index].ans;
-
-     for (var i = 0; i < answers.length; i++) {
-
-          var btn = document.createElement("button");
-          btnsDiv.appendChild(btn);
-          btn.setAttribute("type", "button");
-          btn.setAttribute("data-answer", (i+1));
-          btn.textContent = answers[i];
-
-          btn.addEventListener("click", function(){
-
-               if(this.dataset.answer === quesArr[index].correctAns){
-                    alert("Correct!!");
-               }
-               else{
-                    alert("Wrong Answer!!")
-                    timer -= 10;
-               }
-               this.parentElement.parentElement.classList.add("hide");
-               index++;
-
-               if(index !== quesArr.length)
-                    createAndLoadQues();
-          });
-
+/* Question displayed */
+function questionPage() {
+     if (questionCounter > 5) {
+          clearInterval(timeInterval);
+          submitPage();
      }
-};
 
+     /*Click for answer */
+     let q = questions[currentQuestion];
+     question.innerHTML = q.question;
+     answer1.innerHTML = q.answer1;
+     answer2.innerHTML = q.answer2;
+     answer3.innerHTML = q.answer3;
+     answer4.innerHTML = q.answer4;
+}
 
-start.addEventListener("click", function(){
-     homepage.classList.add("hide");
-     startTime();
-     createAndLoadQues();
-});
-
-highscore.addEventListener("click", function(){
-
-     var allScores = JSON.parse(localStorage.getItem("score"));
-
-     for (var i = 0; i < allScores.length; i++) {
-          alert("Highscores\n"+allScores[i].name +": "+allScores[i].score);
+/* Check if the answer is correct,wrong,or if the questions is timed out */
+function checkAnswer(answer){
+     if (answer === questions[currentQuestion].correct){
+          score++;
+          questionCounter++;
+          setCounterText();
+     } else {
+          questionCounter++;
      }
-});
 
-startQuizButton.addEventListener("click,startQuiz");
+     if (currentQuestion < questionCounter) {
+          currentQuestion++;
+          questionPage();
+     }
+}
+
+/* Display scores */
+function setCounterText() {
+     scoreEl.textContent = "Score: " + score;
+ }
+
+ /* Timer display */
+function timer() {
+     timeLeft = 75;
+     timeInterval = setInterval(function() {
+         if (timeLeft >= 0) { 
+             timerEl.innerText = "Time: " + timeLeft;
+             timeLeft--
+         } else {
+             clearInterval(timeInterval);
+             submissionScreen();
+         }
+     }, 1000);
+ }
+/* Displays home page*/
+function homePage() {
+     introScreen.style.display = "flex";
+     quizScreen.style.display = "none";
+     submitScreen.style.display = "none";
+     highscoreScreen.style.display = "none";
+     scoreEl.style.display = "none";
+     timerEl.style.display = "none";
+}
+/* Initals inserted here */
+function submitPage() {
+     finalscoreEl.innerHTML = score;
+     introScreen.style.display = "none";
+     quizScreen.style.display = "none";
+     submitScreen.style.display = "flex";
+     highscoreScreen.style.display = "none";
+     scoreEl.style.display = "none";
+     timerEl.style.display = "none";
+}
+
+/* Check the highest score of each player */ 
+function highscorePage() {
+     for(let i = 0; i < localStorage.length; i++) {
+          const key = localStorage.key(i)
+          const value = localStorage.getItem(key)
+          highscoreList.innerHTML += `${key}: ${value} <br>`;
+      }
+     introScreen.style.display = "none";
+     quizScreen.style.display = "none";
+     submitScreen.style.display = "none";
+     highscoreScreen.style.display = "flex";
+     scoreEl.style.display = "none";
+     timerEl.style.display = "none";
+}
+
+/*
+submitBtn.onclick = function() {
+     const name = initialEl.value;
+     const finalScore = finalscoreEl.textContent;
+     if (name) {
+         localStorage.setItem(name, finalScore);
+         homePage();
+     }
+ }
+
+clearBtn.onclick = function() {
+     localStorage.clear();
+     location.reload();
+}
+
+startBtn.addEventListener("click", startQuiz);
+highscoreBtn.addEventListener("click", highscorePage);
+homeBtn.addEventListener("click", homePage);
